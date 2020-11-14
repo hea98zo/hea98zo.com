a35a3a17622742629ff62ebdd35ff40a686887f8
jeongsik <jeongsik@naver.com>
2020-11-14 17:50:36 +0900

    2번째

---
+++ company.sql
@@ -0,0 +1,19 @@
+create database company;
+
+use company;
+
+create table dept(
+	id varchar(10),
+	name varchar(50),
+	chief varchar(10)
+);
+
+insert into dept values('001', '기획부', '홍길동');
+
+insert into dept values('002', '총무부', '이경성');
+insert into dept values('003', '인사부', '한이성');
+
+insert into dept values('004', '경리부', '김성경');
+
+select * from dept;
+
